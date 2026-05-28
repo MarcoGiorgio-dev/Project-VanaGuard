df60666f6c707eababadd65b95feb65533b9463e
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-28 10:02:29 +0200

    Refactor camera feed functions for better management
    Refactored camera feed management functions and removed redundant code.

--- videoFeed.py
+++ videoFeed.py
@@ -1,30 +1,41 @@
 from camera import Camera
 import cv2
 
-camera = Camera()
+camera = None
 
-def live_feed():
-    frame = camera.get_frame()
-
-    if frame is None:
-        return None
-    
-    cv2.waitKey(1)
-    cv2.imshow("Kamera Test", frame)
-
-    return frame
-
-def kill_feed():
-    camera.stop()
-    cv2.destroyAllWindows()
-    
 def start_feed():
+    global camera
+    camera = Camera()
     camera.start()
 
+def kill_feed():
+    global camera
+    if camera is not None:
+        camera.stop()
+        camera = None
+    cv2.destroyAllWindows()
+
+def live_feed():
+    if camera is None:
+        return None
+    frame = camera.get_frame()
+    if frame is None:
+        return None
+    cv2.waitKey(1)
+    return frame
+
 def get_processed_frame():
+    if camera is None:
+        return None
     frame = camera.get_frame()
     if frame is None:
         return None
     frame_resized = cv2.resize(frame, (640, 640))
-    frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
-    return frame_rgb
+    return frame_resized
+
+def close_camera():
+    global camera
+    if camera is not None:
+        camera.stop()
+        camera = None
+    cv2.destroyAllWindows()
